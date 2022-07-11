5c207bd740397b0880e6be807109d9ac7c2ec801
BradRice2202 <bradley.rice2202@gmail.com>
2022-07-11 13:30:55 +0200

    fixed depreciated functionality

--- Tina4/PostgresqlBlobHandler.php
+++ Tina4/PostgresqlBlobHandler.php
@@ -20,18 +20,20 @@ class PostgresqlBlobHandler extends DataConnection
     final public function decodeBlobs($record)
     {
         foreach ($record as $key => $value) {
-            if (strpos($value, "0x") === 0) { //@todo how to know if blob ?
-                pg_query($this->getDbh(), "begin");
-                $handle = pg_lo_open($this->getDbh(), $key, "r");
-                //Find the end of the blob
-                pg_lo_seek($handle, 0, PGSQL_SEEK_END);
-                $size = pg_lo_tell($handle);
-                //Find the beginning of the blob
-                pg_lo_seek($handle, 0, PGSQL_SEEK_SET);
-                //Read the whole blob
-                $content = pg_lo_read($handle, $size);
-                pg_query($this->getDbh(), "commit");
-                $record[$key] = $content;
+            if ($value != null){
+                if (strpos($value, "0x") === 0) { //@todo how to know if blob ?
+                    pg_query($this->getDbh(), "begin");
+                    $handle = pg_lo_open($this->getDbh(), $key, "r");
+                    //Find the end of the blob
+                    pg_lo_seek($handle, 0, PGSQL_SEEK_END);
+                    $size = pg_lo_tell($handle);
+                    //Find the beginning of the blob
+                    pg_lo_seek($handle, 0, PGSQL_SEEK_SET);
+                    //Read the whole blob
+                    $content = pg_lo_read($handle, $size);
+                    pg_query($this->getDbh(), "commit");
+                    $record[$key] = $content;
+                }
             }
         }
 
